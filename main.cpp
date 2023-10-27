1a5ab3354838592d47ca5397d1ed30da8f67b55b
silviafpp <silviapacheco724@gmail.com>
2023-10-27 10:29:13 +0000

    AA

--- main.cpp
+++ main.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
-#include <cstdlib>  //para usar o rand() - funcao
-#include <time.h>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
     int tentativas = 0;
     int palpite;
+    string name;
+    srand(time(0)); // Seed the random number generator
 
-    srand(time(0));
-    
-    int numeroSecreto = rand() % 100 + 1; 
+    int numeroSecreto = rand() % 100 + 1;
+
+    cout << "Insira o seu nome antes de jogar: ";
+    cin >> name;
 
     while (true) {
         cout << "Tente adivinhar o numero (entre 1 e 100): ";
         cin >> palpite;
-        tentativas++; //aumenta o valor de "tentivas"
+        tentativas++; // Increase the value of "tentativas"
 
         if (palpite < numeroSecreto) {
             cout << "O numero e muito baixo, tente novamente." << endl;
         } else if (palpite > numeroSecreto) {
             cout << "O numero e muito grande, tente novamente." << endl;
         } else {
-            cout << "Parabens, você acertou o número em " << tentativas << " tentativas!" << endl;
-            break; 
+            cout << "Parabéns " << name << " acertou no número em " << tentativas << " tentativas!" << endl;
+            break;
         }
     }
+
+    return 0;
 }
